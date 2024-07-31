2a15d04cf460539ed2072f05134cb785d374d585
AryanManhas <122689335+AryanManhas@users.noreply.github.com>
2024-07-31 20:28:45 +0530

    Add files via upload

--- ConstJava.cpp
+++ ConstJava.cpp
@@ -1,20 +1,51 @@
-class Solution {
+class Solution
+{
 public
-    int findMaxConsecutiveOnes(int[] nums) {
+    int findMaxConsecutiveOnes(int[] nums)
+    {
         int ans = 0;
-         int count=0;
-    for (int i = 0 ;i<nums.length;i++) {
-        if (nums[i] == 0){
-            count=0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (nums[i] == 0)
+            {
+                count = 0;
             }
             else
             {
                 count++;
             }
-            if(count>ans){
-                ans=count;
+            if (count > ans)
+            {
+                ans = count;
             }
+        }
+        return ans;
     }
-    return ans;
-    }  
+};
+
+class Solution
+{
+public
+    int findMaxConsecutiveOnes(int[] nums)
+    {
+        int ans = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (nums[i] == 0)
+            {
+                count = 0;
+            }
+            else
+            {
+                count++;
+            }
+            if (count > ans)
+            {
+                ans = count;
+            }
+        }
+        return ans;
+    }
 };
